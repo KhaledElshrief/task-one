4bd4b8d931346e5e0f8f27176f9af2dbb33f615d
KhaledElshrief <98036148+KhaledElshrief@users.noreply.github.com>
2024-06-06 23:02:10 +0300

    Add files via upload
    Task Two

---
+++ Task Two/script.js
@@ -0,0 +1,13 @@
+// JavaScript for Design 1
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const email = document.getElementById("email").value;
+        alert(`Thank you for contacting us! We'll get back to you at ${email}.`);
+        contactForm.reset();
+    });
+});
